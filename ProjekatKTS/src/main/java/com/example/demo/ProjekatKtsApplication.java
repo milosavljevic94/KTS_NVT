8c7fdcbe8e2d47f3dd18970bd5c35f423249720d
Nemanja Milosavljevic <nemanjam2008@gmail.com>
2018-11-05 18:49:48 +0100

    Postavka projekta.

---
+++ ProjekatKTS/src/main/java/com/example/demo/ProjekatKtsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjekatKtsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjekatKtsApplication.class, args);
+	}
+}
---
+++ ProjekatKTS/src/main/java/com/example/demo/model/Stajaliste.java
@@ -0,0 +1,107 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+
+/**
+ * @author Nemanja
+ * Nov 5, 2018
+ */
+@Entity
+@Table(name = "stajaliste")
+public class Stajaliste implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String naziv;
+	
+	private Double lokacijaX;
+	
+	private Double lokacijaY;
+	
+	private String adresa;
+
+	
+	public Stajaliste() {
+		super();
+	}
+
+	/**
+	 * @param id
+	 * @param naziv
+	 * @param lokacijaX
+	 * @param lokacijaY
+	 * @param adresa
+	 */
+	public Stajaliste(Long id, String naziv, Double lokacijaX, Double lokacijaY, String adresa) {
+		super();
+		this.id = id;
+		this.naziv = naziv;
+		this.lokacijaX = lokacijaX;
+		this.lokacijaY = lokacijaY;
+		this.adresa = adresa;
+	}
+
+	
+	public Long getId() {
+		return id;
+	}
+
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	
+	public String getNaziv() {
+		return naziv;
+	}
+
+	
+	public void setNaziv(String naziv) {
+		this.naziv = naziv;
+	}
+
+	
+	public Double getLokacijaX() {
+		return lokacijaX;
+	}
+
+	
+	public void setLokacijaX(Double lokacijaX) {
+		this.lokacijaX = lokacijaX;
+	}
+
+	public Double getLokacijaY() {
+		return lokacijaY;
+	}
+
+	
+	public void setLokacijaY(Double lokacijaY) {
+		this.lokacijaY = lokacijaY;
+	}
+
+	
+	public String getAdresa() {
+		return adresa;
+	}
+
+	
+	public void setAdresa(String adresa) {
+		this.adresa = adresa;
+	}
+
+	@Override
+	public String toString() {
+		return "Stajaliste [id=" + id + ", naziv=" + naziv + ", lokacijaX=" + lokacijaX + ", lokacijaY=" + lokacijaY
+				+ ", adresa=" + adresa + "]";
+	}
+	
+	
+	
+
+}
