838f38338e905018fb8873205d85580fa7715be6
Ivan Mihajlov <mihajlov.ivan94@gmail.com>
2018-11-07 02:59:33 +0100

    Bug fix

--- ProjekatKTS/src/main/java/com/example/demo/services/AdministratorServis.java
+++ ProjekatKTS/src/main/java/com/example/demo/services/AdministratorServis.java
@@ -3,10 +3,12 @@ package com.example.demo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Administrator;
 import com.example.demo.repositories.AdministratorRepozitorijum;
 
+@Service
 public class AdministratorServis {
 	@Autowired
 	AdministratorRepozitorijum adminRepo;
--- ProjekatKTS/src/main/java/com/example/demo/services/ZaposleniServis.java
+++ ProjekatKTS/src/main/java/com/example/demo/services/ZaposleniServis.java
@@ -3,10 +3,12 @@ package com.example.demo.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.demo.model.Zaposleni;
 import com.example.demo.repositories.ZaposleniRepozitorijum;
 
+@Service
 public class ZaposleniServis {
 	@Autowired
 	ZaposleniRepozitorijum zaposleniRepo;
