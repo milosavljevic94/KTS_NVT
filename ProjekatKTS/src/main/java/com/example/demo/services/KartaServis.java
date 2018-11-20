1c1e46cc2f1ecdcb005b89534b9f33df9e49eef3
Djapeghn <pkaljevic01@gmail.com>
2018-11-20 13:36:24 +0100

    Zavrsen kontroler (vrati sve,nadji jednu,dodaj,izmeni, obrisi) za kartu.

---
+++ ProjekatKTS/src/main/java/com/example/demo/services/KartaServis.java
@@ -0,0 +1,34 @@
+package com.example.demo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Karta;
+import com.example.demo.repositories.KartaRepozitorijum;
+
+@Service
+public class KartaServis {
+
+	@Autowired
+	KartaRepozitorijum kartaRepo;
+
+	public List<Karta> findAll() {
+		return kartaRepo.findAll();	
+	}
+
+	public Karta getOne(Long id) {
+		return kartaRepo.getOne(id);
+	}
+
+	public Karta save(Karta karta) {
+		return kartaRepo.save(karta);
+	}
+
+	public void delete(Long id) {
+		kartaRepo.deleteById(id);
+		//kartaRepo.delete(karta);	
+	}
+	
+}
