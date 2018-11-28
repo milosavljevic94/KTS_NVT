ac33f955c9f21517e734b66aaf78b4de54723a7d
Djapeghn <pkaljevic01@gmail.com>
2018-11-28 14:35:11 +0100

    Par testova za KartaServis.

--- ProjekatKTS/src/main/java/com/example/demo/repositories/KartaRepozitorijum.java
+++ ProjekatKTS/src/main/java/com/example/demo/repositories/KartaRepozitorijum.java
@@ -1,5 +1,7 @@
 package com.example.demo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.example.demo.model.Karta;
 
 @Repository
 public interface KartaRepozitorijum extends JpaRepository<Karta, Long> {
-
+	
 }
---
+++ ProjekatKTS/src/test/java/com/example/demo/services/KartaServisTest.java
@@ -0,0 +1,55 @@
+package com.example.demo.services;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Date;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.model.Karta;
+import com.example.demo.model.Korisnik;
+import com.example.demo.model.TipKarte;
+import com.example.demo.repositories.KartaRepozitorijum;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class KartaServisTest {
+
+	@Autowired
+	private KartaServis kartaServis;
+
+	@MockBean
+	private KartaRepozitorijum kartaRepozitorijumMocked;
+
+	@SuppressWarnings("deprecation")
+	@Before
+	public void setUp() {
+		Karta kar = new Karta(1L, TipKarte.mesecna, true, new Date(11,11,2015), new Date (11,12,2015), 1250, new Korisnik());
+		Mockito.when(kartaRepozitorijumMocked.getOne(1L)).thenReturn(kar);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void whenNonExistingId_thenThrowNullPointerException() {
+		Long id = 2L;
+		Karta found = kartaServis.getOne(id);
+
+		assertEquals(id, found.getId());
+	}
+	
+	@Test
+	public void whenExistingId_thenfindKarta() {
+		Long id = 1L;
+		Karta found = kartaServis.getOne(id);
+
+		assertEquals(id, found.getId());
+	}
+	
+}
