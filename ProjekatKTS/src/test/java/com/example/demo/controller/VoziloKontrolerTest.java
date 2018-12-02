944bb2e73152cf2860ed602226ec8b08fed72dc7
zrnkemiki <zrnkemiki@gmail.com>
2018-12-03 00:19:05 +0100

    Test VoziloServis + VoziloKontroler

---
+++ ProjekatKTS/src/test/java/com/example/demo/controller/VoziloKontrolerTest.java
@@ -0,0 +1,51 @@
+package com.example.demo.controller;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.model.Linija;
+import com.example.demo.model.Stajaliste;
+import com.example.demo.model.TipVozila;
+import com.example.demo.model.Vozilo;
+import com.example.demo.services.VoziloServis;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class VoziloKontrolerTest {
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
+	@MockBean
+	private VoziloServis voziloServisMocked;
+	
+	@Before
+	public void setUp() {
+		Mockito.when(voziloServisMocked.getOne(13456L)).thenReturn(new Vozilo(13456L, new Stajaliste(), new Linija(), TipVozila.autobus));
+	}
+	
+	@Test
+	public void testGetVozilo() {
+		ResponseEntity<Vozilo> responseEntity = restTemplate.getForEntity("/api/vozilo?id=123L", Vozilo.class);
+		
+		Vozilo vozilo = responseEntity.getBody();
+		
+		assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+		assertNotNull(vozilo);
+		assertEquals(new Long(13456L), vozilo.getId());
+	}
+
+}
---
+++ ProjekatKTS/src/test/java/com/example/demo/services/VoziloServisTest.java
@@ -0,0 +1,54 @@
+package com.example.demo.services;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.model.Linija;
+import com.example.demo.model.Stajaliste;
+import com.example.demo.model.TipVozila;
+import com.example.demo.model.Vozilo;
+import com.example.demo.repositories.VoziloRepozitorijum;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
+public class VoziloServisTest {
+
+	@Autowired
+	private VoziloServis voziloServis;
+
+	@MockBean
+	private VoziloRepozitorijum voziloRepozitorijumMocked;
+
+	
+	@Before
+	public void setUp() {
+		Vozilo vozilo = new Vozilo(123L, new Stajaliste(), new Linija(), TipVozila.autobus);
+		Mockito.when(voziloRepozitorijumMocked.getOne(123L)).thenReturn(vozilo);
+	}
+
+	@Test(expected=NullPointerException.class)
+	public void whenNonExistingId_thenThrowNullPointerException() {
+		Long id = 2L;
+		Vozilo found = voziloServis.getOne(id);
+
+		assertEquals(id, found.getId());
+	}
+	
+	@Test
+	public void whenExistingId_thenfindVozilo() {
+		Long id = 123L;
+		Vozilo found = voziloServis.getOne(id);
+
+		assertEquals(id, found.getId());
+	}
+	
+}
