9bce9997e738b78a396a527ae819e1643b88aa56
Djapeghn <pkaljevic01@gmail.com>
2019-01-07 18:47:37 +0100

    Uradjene osnove kontrolera za stajaliste u angularu.

---
+++ ProjekatKTS/src/main/resources/static/controllers/stajalisteKontroler.js
@@ -0,0 +1,86 @@
+app.controller('stajalisteKontroler', function($scope, stajalisteFactory, $http, $rootScope, $location, $window, userPersistenceService) {
+	
+	$scope.stajalista = [{id:32, naziv:"naziv1", lokacijaX:"22.2", lokacijaY:"32.2", adresa:"Cara Dusana"}];
+	
+    function init() {
+    	console.log('stajalisteKontroler.Init');
+        stajalisteFactory.getAll().success(function (data2) {
+        	$scope.stajalista = data2;
+		});
+    }
+    
+	init();
+	
+	/*$scope.refresh = function(){
+		stajalisteFactory.getAll()
+	          .success(function(data2){
+	        	  $scope.stajalista = data2;
+	          });
+	}*/
+	
+	
+	$scope.addStajaliste = function(stajaliste) {
+		stajalisteFactory.addStajaliste(stajaliste).then(function(data) {
+			//$scope.refresh();
+			toast('Stajaliste ' + stajaliste.naziv + " dodato.");
+		}).catch(function (response) {
+			//$notify.error(response.msg);
+			toast("Stajaliste sa unetim nazivom vec postoji.");
+		});	
+	};
+	
+	$scope.updateStajaliste = function(stajaliste) {
+		stajalisteFactory.updateLek(stajaliste).then(function(data) {
+			//$scope.refresh();
+			toast('Stajaliste ' + stajaliste.naziv + " azurirano.");
+		}).catch(function (response) {
+			//$notify.error(response.msg);
+			toast("Greska pri azuriranju stajalista.");
+		});	
+	};
+	
+	$scope.deleteStajaliste = function(stajaliste) {
+		stajalisteFactory.deleteStajaliste(stajaliste).then(function(data) {
+			//$scope.refresh();
+			toast('Stajaliste ' + stajaliste.naziv + " obrisano.");
+		}).catch(function (response) {
+			//$notify.error(response.msg);
+			toast("Greska pri brisanju stajalista.");
+		});	
+	};
+	
+	$scope.detailViewStajaliste = function(stajaliste) {
+		if(stajaliste==undefined) {
+            //za ovo treba da se napravi nekakav kuki, koji cuva podatke stajalista koje se trenutno gleda, u slucaju da se odradi refresh stranice
+		}
+		else {
+			$rootScope.detailViewStajaliste = stajaliste;
+			//$location.path('/stajalisteDetails');
+		}
+		
+	};
+	
+	/*$scope.back = function() {
+		$location.path('/displayAllStajalista');
+		//$window.history.back();
+	};*/
+	
+	$scope.saveChanges = function() {
+		$scope.updateStajaliste($rootScope.detailViewStajaliste);
+		$location.path('/displayAllStajalista');
+		//$scope.refresh();
+	};
+	
+	$scope.submit = function() {
+		//$rootScope je vidljivo globalno
+		$scope.stajaliste.id;
+		$scope.stajaliste.naziv;
+        $scope.stajaliste.lokacijaX;
+        $scope.stajaliste.lokacijaY;
+		$scope.stajaliste.adresa;
+		
+		$scope.addStajaliste($scope.stajaliste);
+		$location.path('/displayAllStajalista');
+		//$scope.refresh();
+	};
+});
